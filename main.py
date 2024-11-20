0c157398690c6a47b3848fc6bee6d62b1fca0547
Jorge Cruz <croxport@gmail.com>
2024-11-20 12:57:25 -0500

    Update main.py to use the transactions module

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from blockchain import Blockchain
+from transactions import create_wallet, create_transaction
+
+#create wallets
+alice_wallet = create_wallet()
+bob_wallet = create_wallet()
 
 # Initialize the blockchain
 gold_blockchain = Blockchain()
 print(f"Welcome to the {gold_blockchain.coin_name} Blockchain!")
+print(f"Alice's Wallet: {alice_wallet}")
+print(f"Bob's Wallet: {bob_wallet}")
+
+# Add a transaction
+transaction = create_transaction(alice_wallet, bob_wallet, 10)
+gold_blockchain.add_transaction(transaction["sender"], transaction["receiver"], transaction["amount"])
+
+# Mine the pending transactions
+print(f"Mining transactions...")
+gold_blockchain.mine_pending_transactions("Miner1")
 
 # Add some transactions
 gold_blockchain.add_transaction("Alice", "Bob", 10)
